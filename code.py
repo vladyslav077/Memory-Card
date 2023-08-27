6845938b29ffecf87cb593e44f9907146d69ea41
kurunukoleh <85008916+kurunukoleh@users.noreply.github.com>
2023-08-27 18:28:29 +0300

    урок 2
    ---------

--- code.py
+++ code.py
@@ -1,42 +1,96 @@
+import random
 from PyQt5.QtWidgets import *
+import BAZA
 
 app = QApplication([])
 window = QWidget()
+
 window.resize(500, 500)
+
 mainLine = QVBoxLayout()
-menuBtn = QPushButton("Меню")
-restBtn = QPushButton("Відпочити")
+
+menuBth = QPushButton("Menu")
+restBth = QPushButton("Відпочити")
 timeSpn = QSpinBox()
 timeLbl = QLabel("хвилин")
-
-
+Apple = QPushButton("Відповісти")
+Apple2 = QPushButton("Наступне питання")
+Apple1 = QLabel("Яблуко")
 
 
 firstLine = QHBoxLayout()
-firstLine.addWidget(menuBtn)
-firstLine.addWidget(restBtn)
+firstLine.addWidget(menuBth)
+firstLine.addWidget(restBth)
 firstLine.addWidget(timeSpn)
 firstLine.addWidget(timeLbl)
 mainLine.addLayout(firstLine)
+qLine = QHBoxLayout()
+qLine.addWidget(Apple1)
 
-Apple =QLabel("яблуко")
-mainLine.addWidget(Apple)
+mainLine.addLayout(qLine)
 
 
-answersGroup = QGroupBox("оберіть варіанти")
+answerGroups = QGroupBox("Введи правильну відповідь!")
+answerLine = QVBoxLayout()
 answer1 = QRadioButton("1")
 answer2 = QRadioButton("2")
 answer3 = QRadioButton("3")
 answer4 = QRadioButton("4")
-answersLine = QVBoxLayout()
-answersLine.addWidget(answer1)
-answersLine.addWidget(answer2)
-answersLine.addWidget(answer3)
-answersLine.addWidget(answer4)
-answersGroup.setLayout(answersLine)
+answers = [answer1, answer2, answer3, answer4]
 
-mainLine.addWidget(answersGroup)
+answerLine.addWidget(answer1)
+answerLine.addWidget(answer2)
+answerLine.addWidget(answer3),
+answerLine.addWidget(answer4)
+answerGroups.setLayout(answerLine)
+mainLine.addWidget(answerGroups)
 
+result = QLabel("Результат")
+answerLine.addWidget(result)
+result.hide()
+
+qwertyLine = QHBoxLayout()
+qwertyLine.addWidget(Apple)
+qwertyLine.addWidget(Apple2)
+mainLine.addLayout(qwertyLine)
+
+def setQuest():
+    random.shuffle(answers)
+    Apple1.setText(BAZA.quest[BAZA.currentQuest]["питання"])
+    answers[0].setText(BAZA.quest[BAZA.currentQuest]["Правильна відповідь"])
+    answers[1].setText(BAZA.quest[BAZA.currentQuest]["Правильна відповідь"])
+    answers[2].setText(BAZA.quest[BAZA.currentQuest]["Правильна відповідь"])
+    answers[3].setText(BAZA.quest[BAZA.currentQuest]["Правильна відповідь"])
+
+
+
+
+
+
+
+
+
+
+setQuest()
+
+
+
+
+
+
+
+def showResult():
+    answers[0].hide()
+    answers[1].hide()
+    answers[2].hide()
+    answers[3].hide()
+    result.show()
+    Apple2.show()
+    Apple.hide()
+
+
+
+Apple.clicked.connect(showResult)
 
 window.setLayout(mainLine)
 window.show()
